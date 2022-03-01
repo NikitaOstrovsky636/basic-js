868d6ff330288e5c84cce9a66808a5dcacda0f94
NikitaOstrovsky636 <misha.ostrovsky1234567@gmail.com>
2022-03-02 02:34:45 +0300

    feat: complete simole-chain task

--- src/transform-array.js
+++ src/transform-array.js
@@ -29,11 +29,11 @@ function transform(arr) {
           if(arr[i - 2] === '--discard-next') continue;
           newArr.pop();
       } else if (item === '--double-next') {
-        if(i === arr.length - 1) continue;
+          if(i === arr.length - 1) continue;
 
           newArr.push(arr[i + 1]);       
       } else if (item === '--double-prev') {
-        if(i === 0) continue;
+          if(i === 0) continue;
           if(arr[i - 2] === '--discard-next') continue;
           newArr.push(arr[i - 1]);
       } else {
